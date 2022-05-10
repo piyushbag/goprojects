7a4ddb5d46acb028f9bc56e2830af7d4bd267712
Piyush Bag <pbag@arista.com>
2022-05-09 17:23:51 -0700

    movie crud routers

---
+++ src/go-movies-crud/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gorilla/mux"
+)
+
+type Movies struct {
+	ID       string    `json: ID`
+	ISBN     string    `json: ISBN`
+	Name     string    `json: Name`
+	Director *Director `json: Director`
+}
+
+type Director struct {
+	FirstName string `json: FirstName`
+	LastName  string `json: LastName`
+}
+
+func main() {
+	mux := mux.NewRouter()
+	mux.HandleFunc("/", GetAllMovies()).Methods("GET")
+	mux.HandleFunc("/movies/{id}", GetMovie()).Methods("GET")
+	mux.HandleFunc("/movies", CreateMovie()).Methods("POST")
+	mux.HandleFunc("/movies/{id}", UpdateMovie()).Methods("PUT")
+	mux.HandleFunc("/movies", DeleteMovie()).Methods("DELETE")
+
+	fmt.Printf("Listening at Port 8083...")
+
+}
